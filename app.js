aa406c384d1f1896adf254e145bd8370e6a43d7a
CARLOS EDUARDO SILVA PEREIRA <25132872@CFP127ALUNO.LOCAL>
2025-08-12 16:58:57 -0300

    faltando arrumar o appendChild

---
+++ app.js
@@ -0,0 +1,33 @@
+'use strict'
+
+import funcionarios from './funcionarios.json' with {type: 'json'}
+
+function criarCards(dados) {
+    const container = document.getElementById('container')
+    const card = document.createElement('div')
+    const img = document.createElement(`img`)
+    const nome = document.createElement('h1')
+    const cargo = document.createElement('h2')
+    const pastaImg = "./img/"
+
+    card.classList.add('card');
+    img.src = pastaImg + dados.imagem
+    img.alt = dados.nome
+    nome.textContent = dados.nome 
+    nome.classList.add('nome')
+    cargo.textContent = dados.cargo
+    cargo.classList.add('cargo')
+
+    container.appendChild(card)
+    card.appendChild(img)
+    card.appendChild(nome)
+    card.appendChild(cargo)
+
+    
+}
+
+function carregarDados() {
+    funcionarios.forEach(criarCards)
+}
+
+carregarDados()
